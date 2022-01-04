6acb51266d15f04b068abec695722ddb1c5b6b71
Dante Frías <dante.frias@gmail.com>
2022-01-03 23:51:42 -0500

    second commit

--- src/context/FeedbackContext.js
+++ src/context/FeedbackContext.js
@@ -2,7 +2,7 @@ import { v4 as uuidv4 } from "uuid";
 
 import { createContext, useState } from "react";
 
-const FeedbackContext = createContext(null);
+const FeedbackContext = createContext( null );
 
 const initialState = [
   {
@@ -23,37 +23,37 @@ const initialState = [
 ];
 
 export const FeedbackProvider = ({ children }) => {
-  const [feedback, setFeedback] = useState(initialState);
-  const [feedbackEdit, setFeedbackEdit] = useState({ item: {}, edit: false });
+  const [ feedback, setFeedback ] = useState( initialState )
+  const [ feedbackEdit, setFeedbackEdit ] = useState({ item: {}, edit: false })
 
   // Add feedback
-  const addFeedback = (newFeedback) => {
+  const addFeedback = newFeedback => {
     newFeedback.id = uuidv4();
-    setFeedback([newFeedback, ...feedback]);
+    setFeedback( [newFeedback, ...feedback] )
   };
 
   // Delete feedback
   const deleteFeedback = (id) => {
     if (window.confirm("Are you sure you want to delete?")) {
-      setFeedback(feedback.filter((item) => item.id !== id));
+      setFeedback( feedback.filter((item) => item.id !== id) )
     }
   };
 
   // Set item to be updated
-  const editFeedback = (item) => {
+  const editFeedback = item => {
     // console.log( item )
     setFeedbackEdit({ item, edit: true });
   };
 
   // Update feedback item
-  const updateFeedback = (id, updItem) => {
+  const updateFeedback = ( id, updItem ) => {
     // console.log(id, updItem)
     // console.log(id)
     // console.log(updItem)
 
     setFeedback(
       // overwrite the item with equal ID
-      feedback.map((item) => (item.id === id ? { ...item, ...updItem } : item))
+      feedback.map( item => (item.id === id ? { ...item, ...updItem } : item) )
     );
   };
 
@@ -68,7 +68,7 @@ export const FeedbackProvider = ({ children }) => {
         updateFeedback
       }}
     >
-      {children}
+      { children }
     </FeedbackContext.Provider>
   );
 };
